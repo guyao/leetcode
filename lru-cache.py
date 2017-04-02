13d24e2b69c5f5f423b1fd92b22919c50c7f1d2f
guyao <guyao@guyao.me>
2017-04-02 22:24:05 +0800

    update

--- lru-cache.py
+++ lru-cache.py
@@ -24,28 +24,91 @@ cache.get(4);       // returns 4
 
 #Double Linked List
 #TODO
+class Node(object):
+    def __init__(self, key, val):
+        self.prev = None
+        self.next = None
+        self.key = key
+        self.val = val
+
+    def __repl__(self):
+        return str(self.key), ": ",str(self.val)
+
 class LRUCache(object):
 
     def __init__(self, capacity):
         """
         :type capacity: int
         """
-        
+        self.capacity = capacity
+        self.key_node = {}
+        self.cache_head = Node(None, None)
+        self.count = 0
 
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
+        if key in self.key_node:
+            node = self.key_node[key]
+            self.delete_node(node)
+            self.add_to_head(node)
+            return node.val
+        else:
+            return -1
         
-
     def put(self, key, value):
         """
         :type key: int
         :type value: int
         :rtype: void
         """
-        
+        if key in self.key_node:
+            node = self.key_node[key]
+            node.val = value
+            self.update_node(node)
+        else:
+            self.count += 1
+            if self.count > self.capacity:
+                node_to_del = self.cache_head.prev
+                self.delete_node(node_to_del)
+                self.count -= 1
+            node = Node(key, value)
+            self.add_to_head(node)
+            self.key_node[key] = node
+
+    def update_node(self, node):
+        self.delete_node(node)
+        self.add_to_head(node)
+
+    def delete_node(self, node):
+        if node.next is not None:
+            prev = node.prev
+            nxt = node.next
+            prev.next = nxt
+            nxt.prev = prev
+        else:
+            prev = node.prev
+            prev.next = None
+        self.key_node.pop(node.key)
+
+    def add_to_head(self, node):
+        if self.cache_head.next is not None:
+            nxt = self.cache_head.next
+            self.cache_head.next = node
+            node.next = nxt
+            nxt.prev = node
+            node.prev = self.cache_head
+        else:
+            self.cache_head.next = node
+            node.prev = self.cache_head
+            self.cache_head.prev = node
+            node.next = self.cache_head
+        self.key_node[node.key] = node
+
+
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
